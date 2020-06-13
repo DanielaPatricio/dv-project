d698cbc3c437a4ac173e5abfbec1025ebfd23266
DanielaPatricio <dapatric@live.com.pt>
2020-06-13 22:50:49 +0100

    new csv, new css classes, updated app

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ import base64
 #importar dataset
 df = pd.read_csv("SAMPLE_ENEM3.csv", delimiter=",", encoding="utf8")
 
+#replace values
+df['EDUCACAO_PAI'] = np.where(df['EDUCACAO_PAI'] == "Nunca estudou ou fundamental incompleto", "Nunca estudou <br> ou fundamental <br> incompleto", df['EDUCACAO_PAI'])
+df['EDUCACAO_MAE'] = np.where(df['EDUCACAO_MAE'] == "Nunca estudou ou fundamental incompleto", "Nunca estudou <br> ou fundamental <br> incompleto", df['EDUCACAO_MAE'])
+
 #df para o mapa
 mapa = df[["SG_UF_RESIDENCIA", "Unidade federativa", "NU_NOTA_TOTAL", "NU_NOTA_REDACAO", "NU_NOTA_CN","NU_NOTA_MT", "NU_NOTA_LC", "NU_NOTA_REDACAO"]]
 
@@ -39,7 +43,7 @@ encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 #variáveis para usar nos dropdown da variável socioeconómica
 x_escolhas = [
-    {"label": "Classe Social", "value": "social"},
+    {"label": "Grupo Social", "value": "social"},
     {"label": "Tipo de Escola", "value": "escola"},
     {"label": "Raça", "value": "raca"},
     {"label": "Nível Educação Pai", "value": "edu_pai"},
@@ -55,6 +59,12 @@ y_escolhas = [
     {"label": "Nota Ciências da Natureza", "value": "natureza"},
     {"label": "Nota Ciências Humanas", "value": "humanas"}
 ]
+
+#determina ordem variaveis boxplot
+cat = ("Classe A e B", "Classe C", "Classe D e E",
+       "Escola Privada", "Parte em Pública e parte em Privada", "Escola Pública",
+       "Branca", "Amarela", "Parda", "Preta", "Indígena", "Não declarado",
+       "Pós-graduação", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Nunca estudou <br> ou fundamental <br> incompleto", "Não sei")
 
 #iniciar app
 app = dash.Dash(__name__)
@@ -106,6 +116,8 @@ app.layout = html.Div([
         ], className='card1 cards'),
 #Div 4 (boxplot)
         html.Div([
+
+            dcc.Markdown("boxplot"),
             dcc.Graph(id="boxplot"),
         ], className='card2 cards'),
     ],className="row"),
@@ -114,12 +126,164 @@ app.layout = html.Div([
 #Div 3 (heatmap & heatmap)
     html.Div([
         html.Div([
-            dcc.Graph(id="map")
+            dcc.Markdown("Heatmap"),
+            dcc.Graph(id="heatmap")
             ], className='card3 cards'),
+        html.Div([
+            dcc.Markdown("Nota Média por Estado"),
+            dcc.Graph(id="map")
+            ], className='card4 cards'),
     ],className="row"),
 ])
 
-#callbackmapa
+# callback boxplot
+@app.callback(Output("boxplot", "figure"),
+              [Input("socioeconomico", "value"),
+               Input("desempenho", "value")])
+def update_figure(selectedx, selectedy):
+    # valores da axis x
+    if "social" in selectedx:
+        x_box = df["GRUPO_SOCIAL"]
+        s = "Grupo Social"
+    if "escola" in selectedx:
+        x_box = df["TIPO_ESCOLA"]
+        s = "Tipo de Escola"
+    if "raca" in selectedx:
+        x_box = df["TP_COR_RACA"]
+        s = "Raça"
+    if "edu_pai" in selectedx:
+        x_box = df["EDUCACAO_PAI"]
+        s = "Educação do Pai"
+    if "edu_mae" in selectedx:
+        x_box = df["EDUCACAO_MAE"]
+        s = "Educação da Mãe"
+
+    # valores da axis y
+    if "total" in selectedy:
+        y_box = df["NU_NOTA_TOTAL"]
+        n = "Nota Total"
+    if "natureza" in selectedy:
+        y_box = df["NU_NOTA_CN"]
+        n = "Nota Ciências Naturais"
+    if "matematica" in selectedy:
+        y_box = df["NU_NOTA_MT"]
+        n = "Nota Matemática"
+    if "linguagem" in selectedy:
+        y_box = df["NU_NOTA_LC"]
+        n = "Nota Linguagens"
+    if "humanas" in selectedy:
+        y_box = df["NU_NOTA_CH"]
+        n = "Nota Ciências Humanas"
+    if "redacao" in selectedy:
+        y_box = df["NU_NOTA_REDACAO"]
+        n = "Nota Redação"
+
+    boxplot = go.Box(x=x_box,
+                     y=y_box,
+                     # orientation="h",
+                     marker=dict(color="rgb(31,158,137)", opacity=0.2))
+
+    layout_box = go.Layout(title=s + " vs " + n,
+                           xaxis=dict(categoryorder="array", categoryarray=cat),
+                           yaxis=dict(zeroline=False,
+                                      gridcolor='rgb(242, 242, 242)'),
+                           plot_bgcolor='rgb(255, 255, 255)',
+                           margin=dict(
+                               l=40,
+                               r=30,
+                               b=80,
+                               t=100
+                           ))
+
+    fig = go.Figure(data=boxplot, layout=layout_box)
+
+    return fig
+
+# callback heatmap
+@app.callback(Output("heatmap", "figure"),
+              [Input("socioeconomico", "value"),
+               Input("desempenho", "value")])
+def update_figure(selectedx1, selectedy1):
+    # valores da axis x
+    if "social" in selectedx1:
+        x_heat = df["GRUPO_SOCIAL"]
+        s = "Grupo Social"
+    if "escola" in selectedx1:
+        x_heat = df["TIPO_ESCOLA"]
+        s = "Tipo de Escola"
+    if "raca" in selectedx1:
+        x_heat = df["TP_COR_RACA"]
+        s = "Raça"
+    if "edu_pai" in selectedx1:
+        x_heat = df["EDUCACAO_PAI"]
+        s = "Educação do Pai"
+    if "edu_mae" in selectedx1:
+        x_heat = df["EDUCACAO_MAE"]
+        s = "Educação do Mãe"
+
+    # valores da axis y
+    if "total" in selectedy1:
+        z_heat = df["NU_NOTA_TOTAL"]
+        disciplina = "Total"
+    if "natureza" in selectedy1:
+        z_heat = df["NU_NOTA_CN"]
+        disciplina = "Ciências da Natureza"
+    if "matematica" in selectedy1:
+        z_heat = df["NU_NOTA_MT"]
+        disciplina = "Matemática"
+    if "linguagem" in selectedy1:
+        z_heat = df["NU_NOTA_LC"]
+        disciplina = "Linguagens e Códigos"
+    if "humanas" in selectedy1:
+        z_heat = df["NU_NOTA_CH"]
+        disciplina = "Ciências Humanas"
+    if "redacao" in selectedy1:
+        z_heat = df["NU_NOTA_REDACAO"]
+        disciplina = "Redação"
+
+    heatmap = px.density_heatmap(df, x=x_heat,
+                                 y=df["Regiao"],
+                                 z=z_heat,
+                                 histfunc="avg",
+                                 color_continuous_scale="Viridis",
+                                 category_orders={"GRUPO_SOCIAL": ["Classe A e B", "Classe C", "Classe D e E"],
+                                                  "TIPO_ESCOLA": ["Escola Privada",
+                                                                  "Parte em Pública e parte em Privada",
+                                                                  "Escola Pública"],
+                                                  "TP_COR_RACA": ["Branca", "Amarela", "Parda", "Preta", "Indígena",
+                                                                  "Não declarado"],
+                                                  "EDUCACAO_PAI": ["Pós-graduação", "Ensino Superior", "Ensino Médio",
+                                                                   "Ensino Fundamental",
+                                                                   "Nunca estudou <br> ou fundamental <br> incompleto",
+                                                                   "Não sei"],
+                                                  "EDUCACAO_MAE": ["Pós-graduação", "Ensino Superior", "Ensino Médio",
+                                                                   "Ensino Fundamental",
+                                                                   "Nunca estudou <br> ou fundamental <br> incompleto",
+                                                                   "Não sei"]},
+                                 labels={"GRUPO_SOCIAL": "Classe Social", "TIPO_ESCOLA": "Tipo de Escola",
+                                         "TP_COR_RACA": "Raça", "EDUCACAO_PAI": "Nível Educação Pai",
+                                         "EDUCACAO_MAE": "Nível Educação Mãe",
+                                         "NU_NOTA_TOTAL": "Nota Final", "NU_NOTA_CN": "Nota Ciências da Natureza",
+                                         "NU_NOTA_CH": "Nota Ciências Humanas", "NU_NOTA_REDACAO": "Nota Redação",
+                                         "NU_NOTA_LC": "Nota Linguagens e Códigos", "NU_NOTA_MT": "Nota Matemática"
+                                         }
+                                 )
+
+
+
+    title2 = "Nota Média " + "<br>" + disciplina
+
+    fig2 = go.Figure(data=heatmap)
+
+    fig2.update_layout(coloraxis_colorbar=dict(
+        title=title2))
+
+    fig2.update_xaxes(title_text="", side="top")
+    fig2.update_yaxes(title_text="")
+
+    return fig2
+
+#callback mapa
 @app.callback(Output("map", "figure"),
              [Input("desempenho", "value")])
 
@@ -130,36 +294,44 @@ def update_figure(selectedy):
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_TOTAL"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_TOTAL"]),2)
+        disciplina = "Total"
 
     if "natureza" in selectedy:
         y_1 = round(df_mapa["NU_NOTA_CN"], 2)
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_CN"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_CN"]),2)
+        disciplina = "Ciências da Natureza"
 
     if "matematica" in selectedy:
         y_1 = round(df_mapa["NU_NOTA_MT"], 2)
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_MT"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_MT"]),2)
+        disciplina = "Matemática"
 
     if "linguagem" in selectedy:
         y_1 = round(df_mapa["NU_NOTA_LC"], 2)
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_LC"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_LC"]),2)
+        disciplina = "Linguagens e Códigos"
 
     if "humanas" in selectedy:
         y_1 = round(df_mapa["NU_NOTA_CH"], 2)
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_CH"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_CH"]),2)
+        disciplina = "Ciências Humanas"
 
     if "redacao" in selectedy:
         y_1 = round(df_mapa["NU_NOTA_REDACAO"], 2)
         x = df_mapa["Unidade federativa"]
         minmed = round(min(df_mapa["NU_NOTA_REDACAO"]),2)
         maxmed = round(max(df_mapa["NU_NOTA_REDACAO"]),2)
+        disciplina = "Redação"
+
+    title2 = "Nota Média " + "<br>" + disciplina
 
 #configuração e apresentação do mapa
     return px.choropleth_mapbox(df_mapa, geojson=data_geo, locations=x, color=y_1,
@@ -167,64 +339,10 @@ def update_figure(selectedy):
                                 range_color=(minmed, maxmed),
                                 mapbox_style="carto-positron",
                                 center={"lat": -15.4357, "lon": -53.8510},
-                                title=dict(text="Nota Média por Estado",
-                                           x=0.5),
                                 zoom=2,
                                 opacity=0.5,
-                                labels={"locations": "Estado", "color": "Nota Média"}
-
+                                labels={"locations": "Estado", "color": title2}
                                 )
-
-#callbackboxplot
-@app.callback(Output("boxplot", "figure"),
-             [Input("socioeconomico", "value"),
-             Input("desempenho", "value")])
-
-def update_figure(selectedx, selectedy):
-
-#valores da variável de socioeconómica
-    if "social" in selectedx:
-        x_2 = df["GRUPO_SOCIAL"]
-    if "escola" in selectedx:
-        x_2 = df["TIPO_ESCOLA"]
-    if "raca" in selectedx:
-        x_2 = df["TP_COR_RACA"]
-    if "edu_pai" in selectedx:
-        x_2 = df["EDUCACAO_PAI"]
-    if "edu_mae" in selectedx:
-        x_2 = df["EDUCACAO_MAE"]
-
-#valores da variável de desempenho
-    if "total" in selectedy:
-        y_2 = df["NU_NOTA_TOTAL"]
-    if "natureza" in selectedy:
-        y_2 = df["NU_NOTA_CN"]
-    if "matematica" in selectedy:
-        y_2= df["NU_NOTA_MT"]
-    if "linguagem" in selectedy:
-        y_2 = df["NU_NOTA_LC"]
-    if "humanas" in selectedy:
-        y_2 = df["NU_NOTA_CH"]
-    if "redacao" in selectedy:
-        y_2 = df["NU_NOTA_REDACAO"]
-
-    return px.box(df,
-            x=y_2,
-            y=x_2,
-            color=x_2,
-            points="outliers",
-            orientation="h", #se quisermos o normal, tirar essa parte e mudar x e y
-            #range_x=[100,1000],
-            boxmode="overlay",
-            category_orders={"GRUPO_SOCIAL": ["Classe A e B", "Classe C", "Classe D e E"],
-                             "TIPO_ESCOLA": ["Escola Privada", "Parte em Pública e parte em Privada", "Escola Pública"],
-                             "TP_COR_RACA": ["Branca", "Amarela", "Parda", "Preta", "Indígena", "Não declarado"],
-                             "EDUCACAO_PAI": ["Pós-graduação", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Nunca estudou ou fundamental incompleto", "Não sei"],
-                             "EDUCACAO_MAE": ["Pós-graduação", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Nunca estudou ou fundamental incompleto", "Não sei"]},
-            labels={"GRUPO_SOCIAL": "Classe Social",  "TIPO_ESCOLA": "Tipo de Escola", "TP_COR_RACA": "Raça", "EDUCACAO_PAI":"Nível Educação Pai", "EDUCACAO_MAE":"Nível Educação Mãe",
-                    "NU_NOTA_TOTAL":"Nota Final", "NU_NOTA_CN":"Nota Ciências da Natureza", "NU_NOTA_CH":"Nota Ciências Humanas", "NU_NOTA_REDACAO":"Nota Redação",
-                    "NU_NOTA_LC":"Nota Linguagens e Códigos", "NU_NOTA_MT":"Nota Matemática"
-                   })
 
 #run the application
 if __name__ == '__main__':
